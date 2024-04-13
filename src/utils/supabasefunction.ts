6506873d1ab22017c6a71d27d753afe45b778dc1
Yoshito Ikeda <yoshito.i.2002@gmail.com>
2024-04-13 14:16:27 +0900

    add# GoogleAuth

--- src/utils/supabasefunction.ts
+++ src/utils/supabasefunction.ts
@@ -5,3 +5,36 @@ export const fetchTest = async () => {
     const item = await supabase.from('test').select('*');
     return item.data;
 }
+
+
+export async function handleSocialLogin(provider: any) {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    });
+  
+    if (error) {
+      console.log(error);
+      return;
+    }else{
+        console.log(data)
+    }
+  }
+
+  export async function getUser() {
+    const { data: { user } } = await supabase.auth.getUser()
+    
+    if(user){
+      return user.id
+    }else{
+      return 'No User'
+
+    }
+
+  }
+  
