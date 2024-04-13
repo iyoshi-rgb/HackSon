a2eb6814f084d8864e84448c27fc7db527605120
1f10220123 <s1f102201234@iniad.org>
2024-04-13 17:05:41 +0900

    messageの表示

--- src/pages/views/chat.tsx
+++ src/pages/views/chat.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { supabase } from '../../utils/supabase'
+
+interface Messages{
+  UserID:String,
+  Message:String,
+  MessageID:Number,
+  ChatRoomID:Number,
+  Timestamp:Date,
+}
 
 export const Chat = () => {
+
+  const [message,setMessage] = useState<Messages[]>([])
+
+  const fetchMessage = async () => {
+    const messages = (await supabase.from("Messages").select("*")).data;
+    if(!messages){
+      setMessage([])
+    }else{
+    setMessage(messages);}
+  };
+
+  useEffect(() => {
+    fetchMessage()
+  },[])
+  
   return (
-    <div>chat</div>
+    <div>
+      <ul>
+        {message.map((mes,index)=>(
+          <li>{mes.Message}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
