327cd8dcef3e16f8067553e11f2974311ab79627
kakikuu <s2222055@stu.musashino-u.ac.jp>
2024-04-14 13:53:34 +0900

    add:CSSの追加

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -23,45 +23,66 @@ export const Header = () => {
         <header className="flex items-center justify-between py-4">
           <Link
             to="/"
-            className="inline-flex items-center gap-2.5 text-2xl  pl-3 font-bold text-black md:text-3xl"
+            className="inline-flex items-center gap-2.5 text-lg pl-3 font-bold text-black md:text-lg font-sans mx-4"
             aria-label="logo"
           >
             <svg
-              width="95"
-              height="94"
-              viewBox="0 0 95 94"
-              className="h-auto w-6 text-indigo-500"
-              fill="currentColor"
               xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              viewBox="0 0 24 24"
+              fill="#fcc800"
+              stroke="currentColor"
+              stroke-width="1.75"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-heart-handshake"
             >
-              <path d="M96 0V47L48 94H0V47L48 0H96Z" />
+              <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
+              <path d="M12 5 9.04 7.96a2.17 2.17 0 0 0 0 3.08v0c.82.82 2.13.85 3 .07l2.07-1.9a2.82 2.82 0 0 1 3.79 0l2.96 2.66" />
+              <path d="m18 15-2-2" />
+              <path d="m15 18-2-2" />
             </svg>
             まちわたし
           </Link>
 
           <div className="flex-1 items-center  hidden lg:flex">
-            <nav className="flex-1 flex gap-5 ">
-              <Link
+            <nav className="flex-1 flex gap-5">
+              {/* <Link
                 to="/"
-                className="inline-flex items-center gap-1 pl-5 text-lg font-semibold hover:text-cyan-500 active:text-cyan-700"
+                className="inline-flex items-center gap-1 pl-5 hover:text-blue-700 active:text-blue-700"
               >
-                Home
-              </Link>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="35"
+                  height="35"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-home"
+                >
+                  <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                  <polyline points="9 22 9 12 15 12 15 22" />
+                </svg>
+              </Link> */}
               <Link
                 to={"/roomlist"}
-                className="inline-flex items-center gap-1 text-base font-semibold hover:text-indigo-500 active:text-indigo-700"
+                className="inline-flex items-center gap-1 text-sm font-semibold mx-2 hover:text-blue-700 active:text-indigo-700"
               >
-                募集されている部屋
+                募集中の部屋
               </Link>
               <Link
                 to={"/myRooms"}
-                className="inline-flex items-center gap-1 text-base font-semibold hover:text-indigo-500 active:text-indigo-700"
+                className="inline-flex items-center gap-1 text-sm font-semibold mx-2 hover:text-blue-700 active:text-indigo-700"
               >
                 参加済みの部屋
               </Link>
               <Link
                 to={"/makeroom"}
-                className="inline-flex items-center gap-1 text-base font-semibold hover:text-indigo-500 active:text-indigo-700"
+                className="inline-flex items-center gap-1 text-sm font-semibold mx-2 hover:text-blue-700 active:text-indigo-700"
               >
                 募集部屋を作る
               </Link>
@@ -69,7 +90,10 @@ export const Header = () => {
 
             <div className="flex items-center gap-4 pl-5">
               {isLoggedIn ? (
-                <button onClick={handleLogout} className="btn btn-outline">
+                <button
+                  onClick={handleLogout}
+                  className="btn bg-blue-700 text-white rounded-m hover:bg-white hover:text-blue-700 hover:border-blue-700 hover:border-2"
+                >
                   ログアウト
                 </button>
               ) : (
